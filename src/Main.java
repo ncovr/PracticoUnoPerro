24551bbb9525ad906884f1299bc13999810bfa4a
Nicolás Verdugo <nicolas.verdugo2301@alumnos.ubiobio.cl>
2023-09-02 13:49:18 -0400

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+            // Creación de un objeto Perro
+        Perro p1 = new Perro("Murdock", "Pastor Alemán", 2012);
+            // Desplegar los datos de p1
+        System.out.println(p1.toString());
+            // Desplegar la edad que tendrá el proximo año
+        System.out.print("Ingrese año actual: ");
+        int anio = in.nextInt();
+        System.out.println("El perro tendrá: "+(p1.getEdad(anio)+1)+" años el próximo año");
 
+            // Cambiar el nombre de p1 a fiel y edad a +1
+        p1.setNombre("Fiel");
+        p1.setAnioNacimiento(2013);
+
+            // Hacer ladrar a p1 despacio 5 veces
+        for(int i=0; i<5; i++){
+            System.out.println(p1.ladrarDespacio());
+        }
+            // Hacer ladrar a p1 fuerte 3 veces
+        for(int i=0; i<3; i++){
+            System.out.println(p1.ladrarFuerte());
+        }
+
+            // Mostrar los datos de p1
+        System.out.println(p1.toString());
 
     }
 }
--- src/Perro.java
+++ src/Perro.java
@@ -1,2 +1,48 @@
 public class Perro {
+    private String nombre;
+    private String raza;
+    private int anioNacimiento;
+
+    public Perro(String nombre, String raza, int anioNacimiento){
+        this.nombre = nombre;
+        this.raza = raza;
+        this.anioNacimiento = anioNacimiento;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getRaza(){
+        return this.raza;
+    }
+
+    public int getAnioNacimiento(){
+        return this.anioNacimiento;
+    }
+
+    public void setAnioNacimiento(int anio){
+        this.anioNacimiento = anio;
+    }
+
+    public String toString(){
+        return (this.nombre+", "+this.raza+", "+this.anioNacimiento);
+    }
+
+    public int getEdad(int anio){
+        return (anio-this.anioNacimiento);
+    }
+
+    public String ladrarFuerte(){
+        return "GUAU";
+    }
+
+    public String ladrarDespacio(){
+        return "guau";
+    }
+
 }
